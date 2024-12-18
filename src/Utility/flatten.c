8f81fde48680878680bdad20938112fd352da7bc
AdrianvEik <vaneikadrian@gmail.com>
2024-12-18 15:12:12 +0100

    Adjusted exponential flattening to not explode for extreme inputs and to follow a more predictable curve: f(x) = ( exp((x / sum(x)) * a) / max(exp((x / sum(x)) ) + b clamped to [0 1] (TODO: make variable domain edges?)

--- src/Utility/flatten.c
+++ src/Utility/flatten.c
@@ -23,17 +23,45 @@ void lin_flattening(gene_pool_t* gene_pool, flatten_param_t* flatten_param) {
 }
 
 void exp_flattening(gene_pool_t* gene_pool, flatten_param_t* flatten_param) {
+	/*
+    mathematical function:
+        f(x) = ( exp((x / sum(x)) * a) / max(exp((x / sum(x)) ) + b   clamped to [0, 1]
+	where:
+        a = flatten_factor
+        b = flatten_bias
+        x = fitness value
+
+	Note:
+		For large fitness dispersions [fitness_max - fitness_min] the result might stick to the 
+        domain edge [0, 1] for various values close to fitness_max (or fitness_min).
+		To avoid this, use sigmoid flattening.
+	*/
 
 	double sum = 0;
-
 	for (int i = 0; i < gene_pool->individuals; i++) {
 		sum += gene_pool->pop_result_set[i];
 	}
 
-	for (int i = 0; i < gene_pool->individuals; i++) {
-		gene_pool->flatten_result_set[i] = exp((gene_pool->pop_result_set[i] / sum) * flatten_param->flatten_factor) + flatten_param->flatten_bias;
-	}
+	double max_exp = 0.0;
+	// compute normalized exp values
+    for (int i = 0; i < gene_pool->individuals; i++) {
+        gene_pool->flatten_result_set[i] = exp((gene_pool->pop_result_set[i] / sum) * flatten_param->flatten_factor);
+		if (gene_pool->flatten_result_set[i] > max_exp) {
+			max_exp = gene_pool->flatten_result_set[i];
+		}
+    }
 
+    // normalize and bias
+    for (int i = 0; i < gene_pool->individuals; i++) {
+        gene_pool->flatten_result_set[i] = (gene_pool->flatten_result_set[i] / max_exp) + flatten_param->flatten_bias;
+        // clamp to domain [0, 1] TODO: make variable?
+        if (gene_pool->flatten_result_set[i] < 0) {
+            gene_pool->flatten_result_set[i] = 0;
+        }
+        else if (gene_pool->flatten_result_set[i] > 1) {
+            gene_pool->flatten_result_set[i] = 1;
+        }
+    }
 }
 
 void log_flattening(gene_pool_t* gene_pool, flatten_param_t* flatten_param) {
